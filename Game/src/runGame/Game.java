b5af7a504c1ca0fd239636629f1ed5cfec0a3d4b
zeekomkommer <anouaranthonytel@gmail.com>
2024-09-13 15:28:16 +0200

    Imported .java

---
+++ Game/src/runGame/Game.java
@@ -0,0 +1,89 @@
+package runGame;
+
+public class Game
+{
+
+    public static void main(String[] args)
+    {
+	String vakBreaker01_1 = "C";
+	String vakBreaker01_2 = "C";
+	String vakBreaker01_3 = "C";
+	String vakBreaker01_4 = "C";
+	
+	String vakMaster01_1 = "C";
+	String vakMaster01_2 = "C";
+	String vakMaster01_3 = "C";
+	String vakMaster01_4 = "C";
+	
+	String vakBreaker02_1 = "C";
+	String vakBreaker02_2 = "C";
+	String vakBreaker02_3 = "C";
+	String vakBreaker02_4 = "C";
+	
+	String vakMaster02_1 = "C";
+	String vakMaster02_2 = "C";
+	String vakMaster02_3 = "C";
+	String vakMaster02_4 = "C";
+	
+	String vakBreaker03_1 = "C";
+	String vakBreaker03_2 = "C";
+	String vakBreaker03_3 = "C";
+	String vakBreaker03_4 = "C";
+	
+	String vakMaster03_1 = "C";
+	String vakMaster03_2 = "C";
+	String vakMaster03_3 = "C";
+	String vakMaster03_4 = "C";
+	
+	String vakBreaker04_1 = "C";
+	String vakBreaker04_2 = "C";
+	String vakBreaker04_3 = "C";
+	String vakBreaker04_4 = "C";
+	
+	String vakMaster05_1 = "C";
+	String vakMaster05_2 = "C";
+	String vakMaster05_3 = "C";
+	String vakMaster05_4 = "C";
+	
+	String vakBreaker06_1 = "C";
+	String vakBreaker06_2 = "C";
+	String vakBreaker06_3 = "C";
+	String vakBreaker06_4 = "C";
+	
+	String vakMaster06_1 = "C";
+	String vakMaster06_2 = "C";
+	String vakMaster06_3 = "C";
+	String vakMaster06_4 = "C";
+	
+	String vakBreaker07_1 = "C";
+	String vakBreaker07_2 = "C";
+	String vakBreaker07_3 = "C";
+	String vakBreaker07_4 = "C";
+	
+	String vakMaster08_1 = "C";
+	String vakMaster08_2 = "C";
+	String vakMaster08_3 = "C";
+	String vakMaster08_4 = "C";
+	
+	String vakBreaker09_1 = "C";
+	String vakBreaker09_2 = "C";
+	String vakBreaker09_3 = "C";
+	String vakBreaker09_4 = "C";
+	
+	String vakMaster09_1 = "C";
+	String vakMaster09_2 = "C";
+	String vakMaster09_3 = "C";
+	String vakMaster09_4 = "C";
+	
+	String vakBreaker10_1 = "C";
+	String vakBreaker10_2 = "C";
+	String vakBreaker10_3 = "C";
+	String vakBreaker10_4 = "C";
+	
+	String vakMaster10_1 = "C";
+	String vakMaster10_2 = "C";
+	String vakMaster10_3 = "C";
+	String vakMaster10_4 = "C";
+    }
+
+}
