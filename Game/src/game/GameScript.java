49bfbffb21e4670dfcd6928643ccff0015ebc627
zeekomkommer <anouaranthonytel@gmail.com>
2024-09-19 15:29:07 +0200

    EoD Commit

---
+++ Game/src/game/GameScript.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.*;
+
+public class GameScript {
+    public static void main(String[] args) {
+	
+	// Setup Variables
+	final String masterMindLarge =   " ___ ___   ____   _____ ______    ___  ____          ___ ___  ____  ____   ___   \r\n"
+				       + "|   |   | /    | / ___/|      |  /  _]|    \\        |   |   ||    ||    \\ |   \\  \r\n"
+				       + "| _   _ ||  o  |(   \\_ |      | /  [_ |  D  ) _____ | _   _ | |  | |  _  ||    \\ \r\n"
+				       + "|  \\_/  ||     | \\__  ||_|  |_||    _]|    / |     ||  \\_/  | |  | |  |  ||  D  |\r\n"
+				       + "|   |   ||  _  | /  \\ |  |  |  |   [_ |    \\ |_____||   |   | |  | |  |  ||     |\r\n"
+				       + "|   |   ||  |  | \\    |  |  |  |     ||  .  \\       |   |   | |  | |  |  ||     |\r\n"
+				       + "|___|___||__|__|  \\___|  |__|  |_____||__|\\_|       |___|___||____||__|__||_____|\r\n";
+	System.out.println(masterMindLarge);
+    }
+}
